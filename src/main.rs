64449a003777eaa131178ce9ebbe3720d426e59f
Joe <yu.xue.work@outlook.com>
2023-06-06 14:02:51 +0800

    spawn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+use std::collections::VecDeque;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+use futures::task;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let mut mini_tokio = MiniTokio::new();
+
+    mini_tokio.spawn(async {
+        let when = Instant::now() + Duration::from_millis(10);
+        let future = Delay { when };
+
+        let out = future.await;
+        assert_eq!(out, "done");
+    });
+
+    mini_tokio.run();
+}
+
+struct MiniTokio {
+    tasks: VecDeque<Task>,
+}
+
+type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+impl MiniTokio {
+    fn new() -> MiniTokio {
+        MiniTokio {
+            tasks: VecDeque::new(),
+        }
+    }
+
+    /// 生成一个 Future并放入 mini-tokio 实例的任务队列中
+    fn spawn<F>(&mut self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        self.tasks.push_back(Box::pin(future));
+    }
+
+    fn run(&mut self) {
+        let waker = task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+
+        while let Some(mut task) = self.tasks.pop_front() {
+            if task.as_mut().poll(&mut cx).is_pending() {
+                self.tasks.push_back(task);
+            }
+        }
+    }
+}
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            Poll::Ready("done")
+        } else {
+            // 为当然任务克隆一个 waker 的句柄
+            let waker = cx.waker().clone();
+            let when = self.when;
+
+            // 生成一个计时器线程
+            thread::spawn(move || {
+                let now = Instant::now();
+
+                if  now < when {
+                    thread::sleep(when - now);
+                }
+
+                waker.wake();
+            });
+
+            Poll::Pending
+        }
+    }
 }
